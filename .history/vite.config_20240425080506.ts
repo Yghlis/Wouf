5433c596d8b531d4d33af5d4e700653dc37239d3
Youri Ghlis <ghlisyouri@gmail.com>
2024-04-25 16:46:15 +0200

    dsdsdsdsd

--- .history/vite.config_20240425080506.ts
+++ .history/vite.config_20240425080506.ts
@@ -27,6 +27,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
+                tableau: "./src/role/tableau.html",
                 ...getMapsScripts(maps),
             },
         },
